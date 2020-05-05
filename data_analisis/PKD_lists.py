d5220b9a2a18aa613f6d0e50baaa42a134bfb3d6
Agata Nielipińska <agata44316@gmail.com>
2020-05-06 00:28:13 +0200

    Modify PKD lists

--- data_analisis/PKD_lists.py
+++ data_analisis/PKD_lists.py
@@ -15,15 +15,15 @@ for node in tree.findall('poziom'):
         df = df.append(pd.Series([level, symbol, name], index = columns), ignore_index = True)
 
 section_df = df.loc[df['level'] == 'sekcja'][["symbol", "name"]]
-section_df.to_csv('../data/section_list.csv', encoding = "utf-8")
+section_df.to_csv('../data/section_list.csv', encoding = "utf-8", index = False)
 
 division_df = df.loc[df['level'] == 'dział'][["symbol", "name"]]
-division_df.to_csv('../data/division_list.csv', encoding = "utf-8")
+division_df.to_csv('../data/division_list.csv', encoding = "utf-8", index = False)
 
 group_df = df.loc[df['level'] == 'grupa'][["symbol", "name"]]
 group_df["symbol"] = group_df["symbol"].str.replace('.', '', regex=False)
-group_df.to_csv('../data/group_list.csv', encoding = "utf-8")
+group_df.to_csv('../data/group_list.csv', encoding = "utf-8", index = False)
 
 class_df = df.loc[df['level'] == 'klasa'][["symbol", "name"]]
 class_df["symbol"] = class_df["symbol"].str.replace('.', '', regex=False)
-class_df.to_csv('../data/class_list.csv', encoding = "utf-8")
+class_df.to_csv('../data/class_list.csv', encoding = "utf-8", index = False)
